b1f57f5021518c71dd000fadc5a84b85c7253883
aptsharp <afonso.raffael@gmail.com>
2019-12-03 18:26:46 -0300

    CRUD Scaffolding and first migration

---
+++ Data/SellersWebMVCContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SellersWebMVC.Models
+{
+    public class SellersWebMVCContext : DbContext
+    {
+        public SellersWebMVCContext (DbContextOptions<SellersWebMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SellersWebMVC.Models.Department> Department { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using SellersWebMVC.Models;
 
 namespace SellersWebMVC
 {
@@ -33,6 +35,9 @@ namespace SellersWebMVC
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SellersWebMVCContext>(options =>
+                    options.UseMySql(Configuration.GetConnectionString("SellersWebMVCContext"), builder => builder.MigrationsAssembly("SellersWebMVC")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
